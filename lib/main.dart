55491a83623e3e1c35c314eaa5d1b4381fa78e0e
zidanrosyid22 <zidanrosyid22@gmail.com>
2025-09-02 05:09:32 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('PRAKTIKUM MOBILE LANJUT')),
+        body: const Center(
+          child: Text(
+            'Hallo Nama Saya Muhammad Zidan Rosyid',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
